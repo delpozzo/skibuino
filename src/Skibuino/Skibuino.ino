8359d8436d68c46a17db6089cc37e3a51b926fc7
delpozzo <1431725+delpozzo@users.noreply.github.com>
2018-03-20 02:55:17 +0000

    Update Skibuino.ino

--- src/Skibuino/Skibuino.ino
+++ src/Skibuino/Skibuino.ino
@@ -4,7 +4,7 @@
 //    Description: A skiing game for Gamebuino and MAKERbuino.
 //        Version: 1.0.0
 //           Date: 19 Mar 2018
-//        License: GPLv3 (see LICENSE.md)
+//        License: GPLv3 (see LICENSE)
 //
 //    Skibuino Copyright (C) 2018 Mike Del Pozzo
 //    This program is free software: you can redistribute it and/or modify
